162374d9f73335179aad468d0f7722dc20c5ac8e
Som Shrivastava <som.shrivastava@gmail.com>
2024-12-19 15:16:35 -0500

    Created hash tables

---
+++ hash-table.py
@@ -0,0 +1,35 @@
+class HashTable:
+    def __init(self, size = 7):
+        self.data_map = [None] * size # creates a list with 7 items that are all None
+
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash
+    
+    def set(self, key, value):
+        index = self.__hash(key)
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+
+    def get(self, key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i]
+        return None
+    
+    def keys(self):
+        all_keys = []
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys
+
+    def print_table(self):
+        for i, value in enumerate(self.data_map):
+            print(i, ": ", value)
